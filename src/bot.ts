14111de12f18802646e79cf0ea904697cea500ab
JPCastoldi <80767120+JPCastoldi@users.noreply.github.com>
2024-04-22 19:43:47 -0300

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -12,23 +12,21 @@ const bot = new TelegramBot(token, { polling: true });
 bot.on('message', async (msg : any) => {
         const mensagem = msg.text;
         const chatId = msg.chat.id;
-        const date = new Date.Now()
-        const hours = date.getHours();
+        const horas = new Date().getHours();
         
-        if(hours < 9 || hours > 17) 
+        
+        if(horas > 9 && horas < 17) 
         {       
-                if(emailUsuario.includes(chatId)){
-                        cadastrarEmail(mensagem.toString(), chatId.toString());
-                        return;
-                }           
-                
-                bot.sendMessage(chatId, `Nosso horario de funcionamento é de 9h às 18h \nInforme o seu email que entraremos em contato em breve`);
-                emailUsuario.push(chatId);
-                          
+              bot.sendMessage(chatId, "Acesse o site para mais informações: https://faesa.br");     
         }
         else
         {
-                bot.sendMessage(chatId, "Acesse o site para mais informações: https://faesa.br");
+              if(emailUsuario.includes(chatId)){
+                        cadastrarEmail(mensagem.toString(), chatId.toString());
+                        return;
+              }           
+              bot.sendMessage(chatId, `Nosso horario de funcionamento é de 9h às 18h \nInforme o seu email que entraremos em contato em breve`);
+              emailUsuario.push(chatId);
         }
 });
 
